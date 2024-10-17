2e531b1f50498a776b4932919fd2afe3cb3bd085
valletigio <valletigio54@gmail.com>
2024-10-17 12:22:58 +0800

    .

--- src/app/ProfileMenuComponents/ProfilePopUp.tsx
+++ src/app/ProfileMenuComponents/ProfilePopUp.tsx
@@ -48,8 +48,8 @@ const ProfilePopUp: FC<ProfilePopUpProps> = ({ userData, showMenu }) => {
           className='flex items-center justify-center gap-3 w-3/4 h-12 border-gray-300 mt-2 hover:bg-red-400 hover:border-red-400 hover:text-white rounded-b-2xl'
           onClick={() => {signOut() }}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
           </svg>
  
           <p className=' font-semibold'>Sign out</p>
