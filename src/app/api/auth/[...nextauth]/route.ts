93f99e65b160d0a30058245f2a0d08d9dc790adb
valletigio <valletigio54@gmail.com>
2024-10-17 14:09:52 +0800

    .

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,15 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "461642110506-kg29ub68i80jtremtbvt899otcagmg33.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || "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";
-
 const SECRET = process.env.SECRET || "SECRET";
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "461642110506-kg29ub68i80jtremtbvt899otcagmg33.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || "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"; 
+
+// const SECRET = process.env.NEXT_PUBLIC_SECRET !;
+// const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID !;
+// const GOOGLE_CLIENT_SECRET = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET !; 
+
+console.log("test1", process)
 
 const authOption:  NextAuthOptions = {
     session: {
