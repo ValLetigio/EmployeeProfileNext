e22f1e3993b82ebaa8d354af434afcd5eaa3a515
valletigio <valletigio54@gmail.com>
2024-10-17 14:45:48 +0800

    .env file config

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,12 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-const SECRET = process.env.SECRET || "SECRET";
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "461642110506-kg29ub68i80jtremtbvt899otcagmg33.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || "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"; 
+import dotenv from 'dotenv'; 
+dotenv.config(); 
 
-// const SECRET = process.env.NEXT_PUBLIC_SECRET !;
-// const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID !;
-// const GOOGLE_CLIENT_SECRET = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET !; 
-
-console.log("test1", process)
+const SECRET = process.env.SECRET !;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID !;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET !; 
 
 const authOption:  NextAuthOptions = {
     session: {
