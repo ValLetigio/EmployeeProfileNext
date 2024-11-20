b856fe068ba73b0d4a7e9ab2efe386c4bb9034bb
valletigio <valletigio54@gmail.com>
2024-11-20 08:24:13 +0800

    .

--- culori.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'culori' {
-    export function formatRgb(color: any): string;
-    export function parseOklch(color: string): any;
-    export function parse(color: string): any;
-    export function formatHex(color: any): string;
-    export function formatHsl(color: any): string;
-  }
