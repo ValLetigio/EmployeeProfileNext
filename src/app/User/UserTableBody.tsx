9a48a2dfef048c02e5cff1655922095fc67ac7e0
ValLetigio <valletigio54@gmail.com>
2024-12-13 12:15:57 +0800

    .

--- src/app/User/UserTableBody.tsx
+++ src/app/User/UserTableBody.tsx
@@ -2,36 +2,59 @@
 
 import React from "react";
 
-import { User } from "@/app/schemas/UserSchema"; 
+import { User } from "@/app/schemas/UserSchema";
+
+import Image from "next/image";
 
 import { useAppContext } from "../GlobalContext";
 
 interface UserTableBodyProps {
-  userRes: {data: User[], message: string};
-  res: {data: {[k: string]: unknown;}, message: string};
+  userRes: { data: User[]; message: string };
+  res: { data: { [k: string]: unknown }; message: string };
 }
 
 const UserTableBody: React.FC<UserTableBodyProps> = ({ userRes, res }) => {
+  const { userData, serverRequests, router } = useAppContext();
 
-    const { userData, serverRequests, router } = useAppContext();
-     
-  const handleRoleClick = async (user: User, category: string, roleToAdd: string, checked: boolean) => {
-    if(!checked){
-        const res = await serverRequests.addRoleToUser(userData, user, category, roleToAdd);
-        router.refresh()
-        console.log(res);
-    }else{
-        const res = await serverRequests.removeRolefromUser(userData, user, category, roleToAdd);
-        router.refresh()
-        console.log(res);
-    } 
+  const handleRoleClick = async (
+    user: User,
+    category: string,
+    roleToAdd: string,
+    checked: boolean
+  ) => {
+    if (!checked) {
+      const res = await serverRequests.addRoleToUser(
+        userData,
+        user,
+        category,
+        roleToAdd
+      );
+      router.refresh();
+      console.log(res);
+    } else {
+      const res = await serverRequests.removeRolefromUser(
+        userData,
+        user,
+        category,
+        roleToAdd
+      );
+      router.refresh();
+      console.log(res);
+    }
   };
 
-  const checkRole = (roles: string[], userRoles: string[], category:string, user: User) => {
+  const checkRole = (
+    roles: string[],
+    userRoles: string[],
+    category: string,
+    user: User
+  ) => {
     return Object.keys(userRoles).map((key, index) => (
       <div className="flex gap-2 my-2" key={key + index}>
         <input
-          onChange={() => handleRoleClick(user, category, key, roles.includes(key))}
+          onChange={() =>
+            handleRoleClick(user, category, key, roles.includes(key))
+          }
           className="checkbox checkbox-sm"
           type="checkbox"
           name={key}
@@ -46,11 +69,44 @@ const UserTableBody: React.FC<UserTableBodyProps> = ({ userRes, res }) => {
   return (
     <>
       {userRes.data.map((user: User, idx: number) => (
-        <tr key={user._id||"" + idx} className="border-b">
-          <td className="border p-2">{user.displayName}</td>
+        <tr key={user._id || "" + idx} className="border-b">
+          {/* <td className="border p-2">{user.displayName}</td> */}
+          <td>
+          <div className="flex flex-col justify-start items-start gap-3 ">
+            <div className="flex gap-2 items-center justify-center">
+              <div className="mask mask-squircle h-12 w-12 ">
+                {user?.image ? (
+                  <Image
+                    src={user?.image || ""}
+                    loading="lazy"
+                    alt={user?.displayName}
+                    height={100}
+                    width={100}
+                  />
+                ) : (
+                  <div className="h-12 w-12 bg-base-200 grid place-items-center">
+                    {" "}
+                    ?{" "}
+                  </div>
+                )}
+              </div>
+              <div className="font-bold text-lg">{user?.displayName}</div>
+            </div>
+            <div className="text-start">
+              <div className="text-sm opacity-80 tracking-tighter">
+                {user.email ? user.email.toString() : ""}
+              </div>
+            </div>
+          </div>
+          </td>
           {Object?.keys(res?.data || {}).map((key: string) => (
-            <td key={key} className="border p-2  ">
-              {checkRole(user.roles[key] as string[], res.data[key] as string[], key, user)}
+            <td key={key} className="border px-4">
+              {checkRole(
+                user.roles[key] as string[],
+                res.data[key] as string[],
+                key,
+                user
+              )}
             </td>
           ))}
         </tr>
