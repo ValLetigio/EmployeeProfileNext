cc843668e6629a909ea7a8b69b714fa7639c1cfb
Adrian Anton Ladia <ladia.adrian@gmail.com>
2025-01-13 11:27:00 +0800

    updated package.json for vercel

--- src/app/api/AuthOptions.ts
+++ src/app/api/AuthOptions.ts
@@ -12,8 +12,6 @@ const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID !;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET !;
 
 console.log(SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET);    
-console.log(process.env.NEXT_PUBLIC_API_URL);
-
 
 const serverRequests = new ServerRequests( );
 
